ca347c5cfd73758c7b53ab01dfd2649e0ea3eb60
Redko Rostislav <rredko@connecture.com>
2017-01-04 16:53:01 +0200

    Add some stuff for gradle war overlay feature testing

--- src/main/java/com/easytwit/TwitUtilities.java
+++ src/main/java/com/easytwit/TwitUtilities.java
@@ -1,4 +1,12 @@
 package com.easytwit;
 
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
 public class TwitUtilities {
+
+    public ZonedDateTime getCurrentDateTime() {
+        return ZonedDateTime.now(ZoneId.of("ACT"));
+    }
+
 }
